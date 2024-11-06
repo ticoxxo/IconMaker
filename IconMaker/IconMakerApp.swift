274ec13355f8346cac9252856b8d5bee6831ffe6
Ticoxxo <ticoxxo@gmail.com>
2024-11-05 17:50:56 -0700

    Initial Commit

---
+++ IconMaker/IconMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IconMakerApp.swift
+//  IconMaker
+//
+//  Created by Alberto Almeida on 05/11/24.
+//
+
+import SwiftUI
+
+@main
+struct IconMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
